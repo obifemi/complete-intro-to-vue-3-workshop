0af6ac8c4196f75a52b8c0fb85e2e355efc40a70
ali mert inal <mert.inal@gmail.com>
2023-05-15 00:19:23 +0200

    add v-for

---
+++ exercise1/exercise1/src/components/Artists.vue
@@ -0,0 +1,47 @@
+<script>
+export default {
+  name: 'artists',
+ data(){
+    return{
+      artists: [
+        {name: 'Adele'},
+        {name: 'Beyonce'},
+        {name: 'Coldplay'},
+        {name: 'David Bowie'},
+        {name: 'Ed Sheeran'},
+        {name: 'Foo Fighters'},
+        {name: 'Guns N Roses'},
+        {name: 'Hozier'},
+        {name: 'Imagine Dragons'},
+        {name: 'Justin Bieber'},
+        {name: 'Katy Perry'},
+        {name: 'Lady Gaga'},
+        {name: 'Muse'},
+        {name: 'Nirvana'},
+        {name: 'One Direction'},
+        {name: 'Prince'},
+        {name: 'Queen'},
+        {name: 'Rihanna'},
+        {name: 'Sia'},
+        {name: 'Taylor Swift'},
+        {name: 'U2'},
+        {name: 'Van Halen'},
+        {name: 'Whitney Houston'},
+        {name: 'X Ambassadors'},
+        {name: 'Zayn Malik'}
+      ]
+    }
+  }
+}
+
+</script>
+
+<template>
+  <div v-if="artists.length>0">
+    <h1>Artists</h1>
+    <ul>
+      <li v-for="(artist,index) in artists" :key="artist">{{ index }}:{{artist.name}}</li>
+     
+    </ul>
+  </div>
+</template>
---
+++ exercise1/exercise1/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Artists from '../components/Artists.vue';
+</script>
+
+<template>
+  <main>
+    <Artists />
+  </main>
+</template>
